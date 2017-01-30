f0fbe502541d5d0e27eea20c90973007fd7a9661
Adi Topaz <aditopaz@Adis-MacBook-Pro-4.local>
2017-01-30 11:34:42 +0200

    news_feed_update04

--- app/src/main/java/com/example/aditopaz/goodo/VolAdapter.java
+++ app/src/main/java/com/example/aditopaz/goodo/VolAdapter.java
@@ -1,12 +1,13 @@
 package com.example.aditopaz.goodo;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -39,17 +40,55 @@ public class VolAdapter extends RecyclerView.Adapter<VolAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
 
-        VolEntry entry = volList.get(position);
+        final VolEntry entry = volList.get(position);
         holder.volImageView.setBackgroundResource(R.mipmap.kidsmiling);
         holder.nameTextView.setText(entry.name);
 
+        final Handler handler = new Handler();
+        new Thread(new Runnable() {
+
+            int volNum = entry.volNum;
+            int volNeeded = entry.volNeeded;
+            int i = 0;
+            int progressStatus = 0;
+
+            public void run() {
+                //holder.progressBar.setIndeterminate(false);
+                holder.progressBar.setMax(volNeeded);
+                while (progressStatus < volNum) {
+
+                    progressStatus += doWork();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Update the progress bar
+                    handler.post(new Runnable() {
+                        public void run() {
+                            holder.progressBar.setProgress(progressStatus);
+                            i++;
+                        }
+                    });
+                }
+            }
+            private int doWork() {
+
+                return i * 2;
+            }
+        }).start();
+
+
+        /*
         // entry background changer
         if (position % 2 == 0)
             holder.itemView.setSelected(true);
         else
             holder.itemView.setSelected(false);
+        */
     }
 
     @Override
@@ -60,22 +99,24 @@ public class VolAdapter extends RecyclerView.Adapter<VolAdapter.MyViewHolder> {
     public void generateData(){
 
         volList = new ArrayList<VolEntry>();
-        volList.add(new VolEntry(0, "ילידים סודנים"));
-        volList.add(new VolEntry(1, "חוף הרצליה"));
-        volList.add(new VolEntry(2, "חלוקת מזון"));
-        volList.add(new VolEntry(3, "גן ילדים"));
+        volList.add(new VolEntry(0, "ילידים סודנים",100, 50));
+        volList.add(new VolEntry(1, "חוף הרצליה", 100, 70));
+        volList.add(new VolEntry(2, "חלוקת מזון", 100,100));
+        volList.add(new VolEntry(3, "גן ילדים", 100, 20));
     }
 
     public class MyViewHolder  extends RecyclerView.ViewHolder {
 
         LinearLayout volImageView;
         TextView nameTextView;
+        ProgressBar progressBar;
 
         public MyViewHolder(View itemView) {
             super(itemView);
 
             volImageView = (LinearLayout) itemView.findViewById(R.id.content_img);
             nameTextView = (TextView) itemView.findViewById(R.id.name_vol_txt);
+            progressBar = (ProgressBar) itemView.findViewById(R.id.progress_bar);
         }
     }
 }
