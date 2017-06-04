1ce738eb795ad7c72dddcd3088538f2280107517
adi71093 <adimarinovsky@gmail.com>
2017-06-04 19:51:08 +0300

    ui

--- app/src/main/java/com/example/aditopaz/goodo/volReg2.java
+++ app/src/main/java/com/example/aditopaz/goodo/volReg2.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by adi on 24/05/2017.
@@ -24,6 +26,15 @@ public class volReg2 extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View arg0)
             {   Intent i = new Intent(getApplicationContext(),volReg3.class);
+
+                EditText description = (EditText) findViewById(R.id.input_description);
+                String sDescription = description.getText().toString();
+
+                if (sDescription.matches("")) {
+                    Toast.makeText(volReg2.this, "אנא מלא שדות ריקים", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 Bundle infoBund = getIntent().getExtras();
                 Log.d("Bundle", infoBund.getString("VOL_NAME").toString());
                 infoBund.putString("DESCRIPTION",description.getText().toString());
